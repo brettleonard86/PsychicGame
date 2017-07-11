a07cd20d37b9b364c8b55c262ed3c3b5636db3ed
brettl <brettl@mba2067.attlocal.net>
2017-07-11 00:30:53 -0500

    keyboard

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -59,9 +59,43 @@ document.onkeyup = function(event) {
     record();
 }
 
-document.getElementById('openKeyboard').addEventListener('tap', function(){
-    var inputElement = document.getElementById('hiddenInput');
-    inputElement.style.visibility = 'visible'; // unhide the input
-    inputElement.focus(); // focus on it so keyboard pops
-    inputElement.style.visibility = 'hidden'; // hide it again
+$('#ipad').keyboard({
+
+    display: {
+        'bksp'   :  "\u2190",
+        'accept' : 'return',
+        'normal' : 'ABC',
+        'meta1'  : '.?123',
+        'meta2'  : '#+='
+    },
+
+    layout: 'custom',
+    customLayout: {
+        'normal': [
+            'q w e r t y u i o p {bksp}',
+            'a s d f g h j k l {enter}',
+            '{s} z x c v b n m , . {s}',
+            '{meta1} {space} {meta1} {accept}'
+        ],
+        'shift': [
+            'Q W E R T Y U I O P {bksp}',
+            'A S D F G H J K L {enter}',
+            '{s} Z X C V B N M ! ? {s}',
+            '{meta1} {space} {meta1} {accept}'
+        ],
+        'meta1': [
+            '1 2 3 4 5 6 7 8 9 0 {bksp}',
+            '- / : ; ( ) \u20ac & @ {enter}',
+            '{meta2} . , ? ! \' " {meta2}',
+            '{normal} {space} {normal} {accept}'
+        ],
+        'meta2': [
+            '[ ] { } # % ^ * + = {bksp}',
+            '_ \\ | ~ < > $ \u00a3 \u00a5 {enter}',
+            '{meta1} . , ? ! \' " {meta1}',
+            '{normal} {space} {normal} {accept}'
+        ]
+    }
+
 });
+
